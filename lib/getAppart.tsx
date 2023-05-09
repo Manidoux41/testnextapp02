8fa55602f9fae5be5d19aaf0d3e50b67b7904a38
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2023-05-09 23:26:40 +0200

    ENIEME MODIF

--- lib/getAppart.tsx
+++
@@ -1,8 +0,0 @@
-export default async function getAppart(appartId: string) {
-
-    const res = await fetch(`http://localhost:3000/api/apparts/${appartId}`)
-
-    if(!res.ok) throw new Error('failed to fetch data')
-
-    return res.json()
-}
