f6f148dd8326276054ce4d7578d36c45bf066720
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2023-05-09 23:25:12 +0200

    MODIF LOCALHOST

--- lib/getAllApparts.tsx
+++ lib/getAllApparts.tsx
@@ -1,6 +1,6 @@
 export default async function getAllApparts() {
 
-    const res = await fetch("http://localhost:3000/api/apparts")
+    const res = await fetch("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json")
     // https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json
     if(!res.ok) throw new Error('Failed to fetch data')
 
--- lib/getAppart.tsx
+++ lib/getAppart.tsx
@@ -1,6 +1,6 @@
 export default async function getAppart(appartId: string) {
 
-    const res = await fetch(`/api/apparts/${appartId}`)
+    const res = await fetch(`http://localhost:3000/api/apparts/${appartId}`)
 
     if(!res.ok) throw new Error('failed to fetch data')
 
