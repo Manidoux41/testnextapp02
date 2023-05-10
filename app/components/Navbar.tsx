da07fe6c8c6702a5c09e69fec83800d67c87a360
Manfred PARBATIA <superflyman90@gmail.com>
2023-05-10 07:20:21 +0200

    add Link to home page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,43 +1,46 @@
-'use client';
+"use client";
 
-import Link from 'next/link'
-import {usePathname} from 'next/navigation';
-import React from 'react'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
-
-    const pathname = usePathname();
-    const navLink = [
-        {
-            name: "Accueil",
-            link: "/"
-        },
-        {
-            name: "A Propos",
-            link: "/about"
-        }
-    ]
+  const pathname = usePathname();
+  const navLink = [
+    {
+      name: "Accueil",
+      link: "/",
+    },
+    {
+      name: "A Propos",
+      link: "/about",
+    },
+  ];
 
   return (
-    <nav className='p-4 w-full mx-auto lg:w-[1240px] flex justify-between items-center '>
-        <h1 className='text-red-400 text-4xl font-medium'>Kasa</h1>
-        <ul className='flex justify-between items-center gap-5 md:gap-14 text-lg md:text-2xl'>
-            {
-                navLink.map(({link, name}) => (
-                    <Link
-                        key={name}
-                        href={link}
-                        className={ ` ${pathname === link ? 'text-red-500 uppercase md:normal-case text-xs md:text-lg underline' : 'text-red-500 uppercase md:normal-case text-xs md:text-lg'}`}
-                    >
-                        {name}
-                    </Link>
-                ))
-            }
-        </ul>
+    <nav className="p-4 w-full mx-auto lg:w-[1240px] flex justify-between items-center ">
+      <Link href="/">
+        <h1 className="text-red-400 text-4xl font-medium">Kasa</h1>
+      </Link>
+      <ul className="flex justify-between items-center gap-5 md:gap-14 text-lg md:text-2xl">
+        {navLink.map(({ link, name }) => (
+          <Link
+            key={name}
+            href={link}
+            className={` ${
+              pathname === link
+                ? "text-red-500 uppercase md:normal-case text-xs md:text-lg underline"
+                : "text-red-500 uppercase md:normal-case text-xs md:text-lg"
+            }`}
+          >
+            {name}
+          </Link>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
